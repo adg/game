99aa823b17a8a4f6d2a63ea58bc2023ab86192b5
Andrew Gerrand <adg@golang.org>
2015-11-25 22:31:50 +1100

    Load textures from a PNG file. Draw a "Gopher" on the screen.

--- game.go
+++ game.go
@@ -7,8 +7,22 @@
 package main
 
 import (
+	"image"
+	"log"
+
+	_ "image/png"
+
+	"golang.org/x/mobile/asset"
 	"golang.org/x/mobile/exp/f32"
 	"golang.org/x/mobile/exp/sprite"
+	"golang.org/x/mobile/exp/sprite/clock"
+)
+
+const (
+	tileWidth, tileHeight = 16, 16 // width and height of each tile
+	tilesX, tilesY        = 16, 16 // number of horizontal tiles
+
+	gopherTile = 1 // which tile the gopher is standing on (0-indexed)
 )
 
 type Game struct {
@@ -20,11 +34,59 @@ func NewGame() *Game {
 }
 
 func (g *Game) Scene(eng sprite.Engine) *sprite.Node {
+	texs := loadTextures(eng)
+
 	scene := &sprite.Node{}
 	eng.Register(scene)
 	eng.SetTransform(scene, f32.Affine{
 		{1, 0, 0},
 		{0, 1, 0},
 	})
+
+	newNode := func(fn arrangerFunc) {
+		n := &sprite.Node{Arranger: arrangerFunc(fn)}
+		eng.Register(n)
+		scene.AppendChild(n)
+	}
+
+	// The gopher.
+	newNode(func(eng sprite.Engine, n *sprite.Node, t clock.Time) {
+		eng.SetSubTex(n, texs[texGopher])
+		eng.SetTransform(n, f32.Affine{
+			{tileWidth, 0, tileWidth * gopherTile},
+			{0, tileHeight, 0},
+		})
+	})
+
 	return scene
+}
+
+type arrangerFunc func(e sprite.Engine, n *sprite.Node, t clock.Time)
+
+func (a arrangerFunc) Arrange(e sprite.Engine, n *sprite.Node, t clock.Time) { a(e, n, t) }
+
+const (
+	texGopher = iota
+)
+
+func loadTextures(eng sprite.Engine) []sprite.SubTex {
+	a, err := asset.Open("placeholder-sprites.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer a.Close()
+
+	m, _, err := image.Decode(a)
+	if err != nil {
+		log.Fatal(err)
+	}
+	t, err := eng.LoadTexture(m)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	const n = 128
+	return []sprite.SubTex{
+		texGopher: sprite.SubTex{t, image.Rect(1+0, 0, n-1, n)},
+	}
 }

43ef6bb2400cff8bbf84d1c4168e7fa7244d19a7
Andrew Gerrand <adg@golang.org>
2015-11-25 21:55:10 +1100

    Add OpenGL-based sprite engine.

--- main.go
+++ main.go
@@ -7,25 +7,78 @@
 package main
 
 import (
+	"time"
+
 	"golang.org/x/mobile/app"
 	"golang.org/x/mobile/event/lifecycle"
 	"golang.org/x/mobile/event/paint"
+	"golang.org/x/mobile/event/size"
+	"golang.org/x/mobile/exp/f32"
+	"golang.org/x/mobile/exp/gl/glutil"
+	"golang.org/x/mobile/exp/sprite"
+	"golang.org/x/mobile/exp/sprite/clock"
+	"golang.org/x/mobile/exp/sprite/glsprite"
+	"golang.org/x/mobile/gl"
 )
 
 func main() {
 	app.Main(func(a app.App) {
+		var glctx gl.Context
+		var sz size.Event
 		for e := range a.Events() {
 			switch e := a.Filter(e).(type) {
 			case lifecycle.Event:
 				switch e.Crosses(lifecycle.StageVisible) {
 				case lifecycle.CrossOn:
 					// App visible.
+					glctx, _ = e.DrawContext.(gl.Context)
+					onStart(glctx)
+					a.Send(paint.Event{})
 				case lifecycle.CrossOff:
 					// App no longer visible.
+					onStop()
+					glctx = nil
 				}
+			case size.Event:
+				sz = e
 			case paint.Event:
+				if glctx == nil || e.External {
+					continue
+				}
+				onPaint(glctx, sz)
 				a.Publish()
+				a.Send(paint.Event{}) // keep animating
 			}
 		}
 	})
+}
+
+var (
+	startTime = time.Now()
+	images    *glutil.Images
+	eng       sprite.Engine
+	scene     *sprite.Node
+)
+
+func onStart(glctx gl.Context) {
+	images = glutil.NewImages(glctx)
+	eng = glsprite.Engine(images)
+	scene = &sprite.Node{}
+	eng.Register(scene)
+	eng.SetTransform(scene, f32.Affine{
+		{1, 0, 0},
+		{0, 1, 0},
+	})
+}
+
+func onStop() {
+	eng.Release()
+	images.Release()
+}
+
+func onPaint(glctx gl.Context, sz size.Event) {
+	glctx.ClearColor(1, 1, 1, 1)
+	glctx.Clear(gl.COLOR_BUFFER_BIT)
+	now := clock.Time(time.Since(startTime) * 60 / time.Second)
+	eng.Render(scene, now, sz)
 }
